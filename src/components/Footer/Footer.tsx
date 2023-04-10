72d37d16f77fbbade3ca320f7280b0b08fd17cb4
Braifz <braian94lp@gmail.com>
2023-04-10 14:07:44 -0300

    created component Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <h1 className="text-center bg-gradient-to-r text-secondary from-primary to-dark">
+        @Cordobyte 2023
+      </h1>
+    </footer>
+  );
+};
