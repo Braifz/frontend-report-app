838d95bf89f5d083c2fcc7ddb83ad1c2022684ff
Braifz <braian94lp@gmail.com>
2023-04-10 14:08:25 -0300

    render component Layout in page index and report

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { NavbarHome } from "@/components/NavbarHome/NavbarHome";
+import { Navbar } from "@/components/Navbar/Navbar";
 import { Presentation } from "@/components/Presentation/Presentation";
+import { Layout } from "@/components/Layout/Layout";
 
 export default function Home() {
   return (
     <>
-      <NavbarHome />
-      <Presentation />
+      <Layout>
+        <Presentation />
+      </Layout>
     </>
   );
 }
