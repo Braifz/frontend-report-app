c12d6b8ef8b38fd0bed6867237fcf7d4327747fd
Braifz <braian94lp@gmail.com>
2023-04-11 17:39:24 -0300

    render component FAQs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import { Navbar } from "@/components/Navbar/Navbar";
 import { Presentation } from "@/components/Presentation/Presentation";
 import { Layout } from "@/components/Layout/Layout";
+import { FAQs } from "@/components/FAQs/FAQs";
 
 export default function Home() {
   return (
     <div>
       <Layout>
         <Presentation />
+        <div className="flex flex-col items-center justify-center w-full  bg-light pt-[100px] pb-[100px]">
+          <h1 className="text-2xl text-dark text-center font-bold w-2/3 mb-[24px] ">
+            Preguntas Frecuentes
+          </h1>
+          <FAQs
+            label="¿Como hacer mi reporte?"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+          <FAQs
+            label="¿Cuando me llegará mi dinero?"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+          <FAQs
+            label="¿Como saber si ya se pago el reporte?"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+          <FAQs
+            label="Pregunta random2"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+        </div>
       </Layout>
     </div>
   );
