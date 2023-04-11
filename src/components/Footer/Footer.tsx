4d42e5ceaeaf2612ceee479cf47308a0e0095ee1
Braifz <braian94lp@gmail.com>
2023-04-11 17:37:27 -0300

    change styles footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="">
       <h1 className="text-center bg-gradient-to-r text-secondary from-primary to-dark">
         @Cordobyte 2023
       </h1>
